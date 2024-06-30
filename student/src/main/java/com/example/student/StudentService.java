542956ca4470c335f529aab33c4d167799297c2f
Mohit Singh <msakra000@gmail.com>
2024-06-30 14:59:21 +0530

    initial commit

---
+++ student/src/main/java/com/example/student/StudentService.java
@@ -0,0 +1,26 @@
+package com.example.student;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository repository;
+
+    public void saveStudent(Student student) {
+        repository.save(student);
+    }
+
+    public List<Student> findAllStudents() {
+        return repository.findAll();
+    }
+
+    public List<Student> findAllStudentsBySchool(Integer schoolId) {
+        return repository.findAllBySchoolId(schoolId);
+    }
+}
